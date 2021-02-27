150cbef483d669ad97ebc91d5c05e9e675763af6
Shivam Sharma <shivam.sharma@berylsystems.com>
2021-02-27 14:49:39 +0530

    More LinkedList function is been added

--- Java_Programs/src/main/java/com/example/java_programs/LlinkedList.java
+++ Java_Programs/src/main/java/com/example/java_programs/LlinkedList.java
@@ -3,6 +3,7 @@ package com.example.java_programs;
 import java.util.LinkedList;
 
 public class LlinkedList {
+
     public static void main(String []args){
         LinkedList<String> linkedList = new LinkedList<String>();
         linkedList.add("Shivam");
@@ -13,11 +14,23 @@ public class LlinkedList {
 
         System.out.println(linkedList);
 
+        LinkedList<String> linkedList1 = (LinkedList) linkedList.clone();
+
+        System.out.println(linkedList1);
+
+        String item = linkedList.peek();  // peek function return first element of the list
+
+        System.out.println("First element of list is ->>"+ item);
+
+         int size   = linkedList.size();
+
+         System.out.println("Return the size of an list"+size); // return the size of this list  //
+
         String s = linkedList.get(1); // used to get the  specific element from the list  //
 
         System.out.println(s);
 
-        linkedList.remove(1);  /// used to removing specific item from list
+        linkedList.remove(1);  /// used to removing  item by index //
 
         System.out.println(linkedList);
 
